35769de861888267ff6d51a1bcc02e02f42db650
ggnsall <ggnsall@naver.com>
2024-04-27 00:48:27 +0900

    [FE] CHOR : npm 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
+import * as path from 'path';
+import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), vanillaExtractPlugin({ identifiers: ({ hash }) => `prefix_${hash}` })],
-	resolve: {
-		alias: [{ find: '@', replacement: '/src' }],
-	},
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/libs/index.ts'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
+  plugins: [react(), vanillaExtractPlugin({ identifiers: ({ hash }) => `prefix_${hash}` }), dts()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
 });
